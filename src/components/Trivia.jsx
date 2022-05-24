8fbfb4066615720603b40b0dade2485ce52d1605
Sudaraka Gamage <ashenchamli220@gmail.com>
2022-05-24 13:50:38 +0530

    Start component

--- src/components/Trivia.jsx
+++ src/components/Trivia.jsx
@@ -1,4 +1,8 @@
-import { useEffect, useState } from "react"
+import { useEffect, useState } from "react";
+import useSound from "use-sound";
+import correct from "../assets/src_sounds_correct.mp3"
+import play from "../assets/src_sounds_play.mp3"
+import wrong from "../assets/src_sounds_wrong.mp3"
 
 export default function Trivia({
   data,
@@ -9,7 +13,14 @@ export default function Trivia({
   const [question, setQuestion] = useState(null);
   const [selectedAnswer, setSelectedAnswer] = useState(null);
   const [className, setClassName] = useState("answer");
+  const [letsPlay] = useSound(play)
+  const [correctAnswer] = useSound(correct)
+  const [wrongAnswer] = useSound(wrong)
 
+
+  useEffect(()=>{
+        letsPlay();
+  },[letsPlay])
 
   useEffect(()=>{ 
     setQuestion(data[questionNumber - 1]);
@@ -27,12 +38,19 @@ export default function Trivia({
     delay(3000, () => 
       setClassName(a.correct ? "answer correct" : "answer wrong")
     );
-    delay(6000, () => {
+    delay(5000, () => {
       if(a.correct) {
+        correctAnswer();
+        delay(1000, ()=>{
           setQuestionNumber((prev) => prev + 1);
           setSelectedAnswer(null);
+        });
+
       }else{
-        setStop(true);
+        wrongAnswer();
+        delay(1000, () => {
+          setStop(true);
+        });
       }
     }
   );
